4e496463bf1969384260fcb40d8b323c0e7d9e6c
lisbangonsalves <lisbangonsalvescontact@gmail.com>
2021-10-17 15:38:22 +0530

    cat

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,8 +1,16 @@
-function myfun1() {
-    var x = document.getElementById("subcat");
+function myfun() {
+    var x = document.querySelector(".cat1-sub")
+    var y = document.querySelector(".cat2-sub")
     if (x.style.display === "none") {
       x.style.display = "block";
+      if (y.style.display ==="block"){
+          y.style.display = "none";
+      }
     } else {
       x.style.display = "none";
     }
   }
+
+  
+
+  
