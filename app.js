3e9caae70df1e973e315abb8e1f58da20143429e
Axill <axilldcunha@gmail.com>
2021-10-17 14:04:19 +0530

    node js and express

---
+++ app.js
@@ -0,0 +1,39 @@
+//Imports
+const express= require("express");
+const bodyParser=require("body-parser");
+const path=require("path");
+const port= process.env.port||3000 
+
+const app=express();
+
+//seting static files
+app.use(express.static("public")); //for servinhg staticfile
+//setting view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//HOMEPAGE 
+app.get("/",(req,res)=>{
+    res.status(200).render('index');
+});
+
+//CATEGORIES
+app.get("/categories",(req,res)=>{
+    res.status(200).render('index');
+});
+
+
+
+
+//LOGIN
+app.get("/login",(req,res)=>{
+    res.status(200).render('index');
+});
+
+
+
+
+//Listening on port 3000
+app.listen(port,()=>{
+    console.log(`sever started on port ${port}`);
+});
