89ecc1213be383c8ebffaf74307d3d99cb11ab63
lisbangonsalves <lisbangonsalvescontact@gmail.com>
2021-10-17 14:07:04 +0530

    yes

---
+++ public/js/categories.js
@@ -0,0 +1,8 @@
+function myfun1() {
+    var x = document.getElementById("subcat");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
