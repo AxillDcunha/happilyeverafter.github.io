bfbbfa31ca38ed74d5f3ed72a28c5eac7c1944da
lisbangonsalves <lisbangonsalvescontact@gmail.com>
2021-10-17 14:36:46 +0530

    3001

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express= require("express");
 const bodyParser=require("body-parser");
 const path=require("path");
-const port= process.env.port||3000 
+const port= process.env.port||3001
 
 const app=express();
 
