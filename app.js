7f7f4f327b9f5399e3d63e9cf54d80f339ae542c
Axill <axilldcunha@gmail.com>
2021-12-07 02:28:27 +0530

    final production

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/happilyeverafter",
+      callbackURL: "https://happilyeveraftergithub.herokuapp.com/auth/google/happilyeverafter",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
